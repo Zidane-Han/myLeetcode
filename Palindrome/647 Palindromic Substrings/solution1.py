22c18896069d96df33a9ac320f83d9b457f715eb
Zidane-Han <35073211+Zidane-Han@users.noreply.github.com>
2019-11-26 19:48:38 +0800

    Create solution1.py

---
+++ Palindrome/647 Palindromic Substrings/solution1.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        def isPalindromic(string):
+            if string == string[::-1]:
+                return True
+            else:
+                return False
+            
+        res = 0
+        for i in range(len(s)+1):
+            for j in range(i+1, len(s)+1):
+                if isPalindromic(s[i:j]):
+                    res += 1
+                    
+        return res
