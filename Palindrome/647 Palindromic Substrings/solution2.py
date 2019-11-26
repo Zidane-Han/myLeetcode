ad860bc6e1950b9d83cbdabea4b544ea6aeb518c
Zidane-Han <35073211+Zidane-Han@users.noreply.github.com>
2019-11-26 19:49:00 +0800

    Create solution2.py

---
+++ Palindrome/647 Palindromic Substrings/solution2.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        res = 0
+        
+        for center in range(2*len(s)-1):
+            left = center // 2
+            right = left + center % 2
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                res += 1
+                left -= 1
+                right += 1
+                
+        return res
