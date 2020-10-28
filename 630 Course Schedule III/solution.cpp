fd35d6a0559ed177633f4db35b6912f9d956a2c8
Zidane-Han <35073211+Zidane-Han@users.noreply.github.com>
2020-10-28 10:17:48 +0800

    Update solution.cpp
    fix TLE error

--- 630 Course Schedule III/solution.cpp
+++ 630 Course Schedule III/solution.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    static bool Compare(vector<int> courseA, vector<int> courseB) {
+    static bool Compare(vector<int>& courseA, vector<int>& courseB) {
         return (courseA[1] < courseB[1]);
     }
 
@@ -8,7 +8,9 @@ public:
         priority_queue<int> pq; // from largest duration to smallest
         // sort courses by end time, from early to late
         sort(courses.begin(), courses.end(), Compare);
-
+        // lambda expression 
+        // sort(courses.begin(), courses.end(), [](vector<int>& A, vector<int>& B){return A[1] < B[1];});
+        
         int currt = 0;
         for (auto course : courses) {
             currt += course[0];
