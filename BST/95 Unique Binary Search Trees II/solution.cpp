4222997a8686eca6db68956227b015e09b56c592
Zidane-Han <han.gao.zidane@gmail.com>
2020-11-15 23:37:10 +0800

    add prob 95 & 96

---
+++ BST/95 Unique Binary Search Trees II/solution.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // divide and conquer
+    vector<TreeNode*> generateTrees(int n) {
+        if (n == 0) return {};
+        
+        return helper(1, n);
+    }
+    
+    vector<TreeNode*> helper(int start, int end) {
+        if (start > end)    return {nullptr};
+        
+        vector<TreeNode*> res;
+        for (int i = start; i <= end; i++) {
+            auto left = helper(start, i-1);
+            auto right = helper(i+1, end);
+            for (auto l : left) {
+                for (auto r : right) {
+                    TreeNode* node = new TreeNode(i);
+                    node->left = l;
+                    node->right = r;
+                    res.push_back(node);
+                }
+            }
+            
+        }
+        
+        return res;
+    }
+};
---
+++ BST/96 Unique Binary Search Trees/README.md
@@ -0,0 +1,32 @@
+# Unique Binary Search Trees (Medium)
+
+## Description
+
+Given `n`, how many structurally unique BST's (binary search trees) that store values 1 ... n?
+
+**Example:**
+
+```html
+Input: 3
+Output: 5
+Explanation:
+Given n = 3, there are a total of 5 unique BST's:
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+```
+
+**Constraints:**
+
+- 0 <= n <= 19
+
+## Idea
+
+refer to Catalan number
+
+https://en.wikipedia.org/wiki/Catalan_number
+
+https://www.youtube.com/watch?v=GgP75HAvrlY
